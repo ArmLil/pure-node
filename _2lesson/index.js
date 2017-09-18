acedbc15a724ca2230e7980d575adabc6c6b03f9
ArmLil <armlilhov@mail.ru>
2017-09-18 19:03:37 +0400

    use pug

--- _2lesson/index.js
+++ _2lesson/index.js
@@ -3,6 +3,7 @@
 const fs = require ('fs')
 const http = require('http')
 const pathModule = require('path')
+const pug = require('pug');
 
 const TWEETS_PATH = './tweets.json'
 const FAVICON = pathModule.join(__dirname, 'public', 'favicon.ico');
@@ -19,7 +20,7 @@ const readBody = (req) => {
   })
 }
 
-const read_file = (path) => {
+const readFile = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) return reject(err);
@@ -28,7 +29,7 @@ const read_file = (path) => {
   });
 }
 
-const write_file = (path, data) => {
+const writeFile = (path, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(path, data, (err) => {
       if (err) return reject(err)
@@ -37,7 +38,7 @@ const write_file = (path, data) => {
   })
 }
 
-const append_file = (path, body) => {
+const appendFile = (path, body) => {
   return new Promise((resolve, reject) => {
     fs.appendFile(path, body, (err) => {
       if (err) return reject(err)
@@ -48,7 +49,7 @@ const append_file = (path, body) => {
 
 const create = (req, path) => {
   return readBody(req)
-  .then((body) => append_file(path, body))
+  .then((body) => appendFile(path, body))
   .then(response => {
     return Object.assign({}, {
       body: {
@@ -68,7 +69,7 @@ const update = (req, path) => {
   let localBody
   return readBody(req)
   .then((body) => localBody = body)
-  .then(() => read_file(path))
+  .then(() => readFile(path))
   .then((data) => {
     let tweetsArray = []
     if(data) tweetsArray = JSON.parse(data).tweets
@@ -78,7 +79,7 @@ const update = (req, path) => {
     const result = JSON.stringify(newTweetsObject, null, '\t')
     return result
   })
-  .then((result) => write_file(path, result))
+  .then((result) => writeFile(path, result))
   .then(response => {
     return Object.assign({}, {
       body: {
@@ -94,6 +95,56 @@ const update = (req, path) => {
   })
 }
 
+//...use with html
+// const makeTweets = (tweets) => {
+//   let tweetsLocal = JSON.parse(tweets).tweets
+//   return tweetsLocal.map(tweet => {
+//     return (`<li>
+//       <h3>tweet:${tweet.tweet}</h3>
+//       <h4>user:${tweet.user}</h4>
+//     </li>`)
+//   })
+// }
+
+const makeTweets = (tweets) => {
+  let tweetsLocal = JSON.parse(tweets).tweets
+  let local = tweetsLocal.map(tweet => {
+      return (`
+ li
+  h3 ${tweet.tweet}
+  h4 user: ${tweet.user}
+  p id: ${tweet.id}\n`)
+    })
+    local = local.toString().replace(/,/g, "")
+    return `
+h1 Our Tweets
+ul
+${local}
+`
+}
+
+const homePage = (req, path) => {
+  return readFile(path)
+  .then(makeTweets)
+  .then(tweets => {
+  //...use with html
+  // tweets = `<html><body>${tweets}</body></html>`
+  // tweets = tweets.replace(/,/g, "")
+
+  const pugTweets = pug.compile(tweets)
+    return Object.assign({}, {
+    //body: tweets //...use with html
+    body: pugTweets()
+  }, {
+    header: {
+      code: 200,
+      type: 'text/html',
+      message: 'GET request',
+    }
+  })
+})
+}
+
 const requestHandle = (req) => {
   const { method, url, body } = req;
   const fileExists = fs.existsSync(TWEETS_PATH)
@@ -104,16 +155,15 @@ const requestHandle = (req) => {
     else if (method === 'PUT' && fileExists)
       return update(req, TWEETS_PATH)
     else if (method === 'GET' && fileExists) {
-      return read_file(TWEETS_PATH)
+      return readFile(TWEETS_PATH)
+      .then(tweets => JSON.parse(tweets))
       .then(response => {
         return Object.assign({}, {
-          body: {
-            message: response
-          }
+          body: response
         }, {
           header: {
             code: 200,
-            type: 'text/plain',
+            type: 'application/json',
             message: 'GET request',
           }
         })
@@ -122,18 +172,7 @@ const requestHandle = (req) => {
     else return Promise.reject('Bad Request')
   }
   else if (url === '/' && method === 'GET') {
-    return Promise
-    .resolve(Object.assign({}, {
-      body: {
-        message: 'GET request'
-      }
-    }, {
-      header: {
-        code: 200,
-        type: 'text/plain',
-        message: 'GET request',
-      }
-    }))
+    return homePage(req, TWEETS_PATH)
   }
   else return Promise.reject('Bad Request')
 }
@@ -153,7 +192,13 @@ server.on('request', (req, res) => {
       res.writeHead(code, message, {
         'Content-Type': type
       })
-      res.end(JSON.stringify(response.body))
+      if( type === 'text/html'){
+        res.write(response.body)
+      } else {
+        res.write(JSON.stringify(response.body))
+      }
+      res.end()
+
     })
     .catch(error => {
       console.error('Opps', error)
@@ -163,6 +208,6 @@ server.on('request', (req, res) => {
     })
 });
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('server listening on port', server.address().port)
 })
